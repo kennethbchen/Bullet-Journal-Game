610118b755613db7df266394c4890c8b58e959a6
kennethbchen <kennethbchen@gmail.com>
2023-07-13 16:42:49 -0400

    Moved credits to the intro screen

--- Scripts/Intro.gd
+++ Scripts/Intro.gd
@@ -4,6 +4,8 @@ extends Node2D
 
 @onready var results_ui: CanvasLayer = $ResultsUI
 @onready var main_ui: CanvasLayer = $GameUI
+@onready var credits_ui: CanvasLayer = $CreditsUI
+
 @onready var controls_label: Label = $GameUI/ControlsLabel
 @onready var done_label: Label = $GameUI/DoneLabel
 @onready var done_button: Button = $GameUI/DoneButton
@@ -13,6 +15,7 @@ signal game_ended()
 func _ready():
 	results_ui.hide()
 	main_ui.show()
+	credits_ui.hide()
 	
 	controls_label.hide()
 	done_label.hide()
@@ -25,8 +28,17 @@ func _on_next_button_pressed():
 func _on_done_button_pressed():
 	results_ui.show()
 	main_ui.hide()
+	credits_ui.hide()
 	
 	game_ended.emit()
+
+func _on_credits_button_pressed():
+	credits_ui.show()
+	main_ui.hide()
+
+func _on_close_credits_button_pressed():
+	credits_ui.hide()
+	main_ui.show()
 
 func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
